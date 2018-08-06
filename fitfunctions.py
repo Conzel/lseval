c8c48b6ca7bf06b9842355b7ee65e593f72c1454
Conzel <38732545+Conzel@users.noreply.github.com>
2018-08-06 14:18:28 +0200

    Added new plotting functionality, splitted code further up into utils and fitfuns.

--- fitfunctions.py
+++ fitfunctions.py
@@ -23,20 +23,34 @@ def doubleExp(x, a1, a2, b1, b2):
     return a1*np.exp(-2*b1*x) + a2*np.exp(-2*b2*x)
 
 
-def cum2(x, a, gammaMean, mu2):
+def cum2(x, a, mu1, mu2):
     """
     Cumulant with three cumulants:
         g1 = a* exp(-gammaMean (x - mu2/2*x**2)
+    mu1 is gamma mean and equals <Ds*q**2>,
+    mu2 is a measure for the standard deviation of the system with
+        sigma = sqrt(mu2/mu1)
     """
-    return a*np.exp(-gammaMean*(x - mu2/2*x**2))
+    return a*np.exp(-mu1*x - mu2/2*x**2)
 
 
-def cum3(x, a, gammaMean, mu2, mu3):
+def cum3(x, a, mu1, mu2, mu3):
     """
     Cumulant with three cumulants:
-        g1 = a* exp(-gammaMean (x - mu2/2*x**2 + mu3/6*x**3))
+        g1 = a* exp(-mu1 (x - mu2/2*x**2 + mu3/6*x**3))
+    mu1 is gamma mean and equals <Ds*q**2>,
+    mu2 is a measure for the standard deviation of the system with
+        sigma = sqrt(mu2/mu1)
+    mu3 stands for skewness.
     """
-    return a*np.exp(-gammaMean*(x - mu2/2*x**2 + mu3/6*x**3))
+    return a*np.exp(-mu1*x + mu2/2*x**2 - mu3/6*x**3)
+
+
+def cum3b(x, a, mu1, mu2, mu3):
+    """
+    Eq. 23 by Barbara Frisken.
+    """
+    return a*np.exp(-2*mu1*x)*(1 + mu2/2*x**2 - mu3/6*x**3)**2
 
 
 def nameOf(function):
@@ -44,6 +58,7 @@ def nameOf(function):
         singleExp: "Single Exponential",
         doubleExp: "Double Exponential",
         cum2: "2-Cumulant",
-        cum3: "3-Cumulant"
+        cum3: "3-Cumulant",
+        cum3b: "3-Cumulant"
     }
     return nameDict[function]
--- utils.py
+++ utils.py
@@ -1,3 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Module containing various utility functions for data retrieval and
+calculations.
+"""
+
+import os
+import re
+import numpy as np
+from constants import *
+
+
 def isFloat(num):
     """
     Returns True if input is a number, otherwise False.
@@ -7,3 +19,76 @@ def isFloat(num):
         return True
     except (TypeError, ValueError):
         return False
+
+
+def getFilesInFolder(path=os.getcwd(), searchReg="_averaged.ASC"):
+    """
+    Returns a list of all filenames from the folder specified in path
+    with the searchString as regular expression.
+    """
+    fileList = os.listdir(path)
+    filteredList = []
+    for name in fileList:
+        if re.match(searchReg, name):
+            filteredList.append(name)
+    return filteredList
+
+
+def match(regExp, string):
+    """
+    returns true if regExp matches string, false otherwise.
+    """
+    return re.search(regExp, string) is not None
+
+
+def getHydroDynR(diffu, visc, temp):
+    """
+    Calculates hydrodynamic radius from inputs, all in SI units.
+
+    diff: Diffusion coefficient
+    visc: dynamic viscosity of solvent
+    temp: Temperature of surroundings.
+    """
+    rhyd = KB * temp / (6 * np.pi * visc * diffu)
+    return rhyd
+
+
+def recursiveCall(function, filter, folder=os.getcwd()):
+    """
+    Calls the function specified recursively on a folder. Opens folder,
+    every file found on the level of the folder, that fits the filter (regExp)
+    gets passed as a list to the function. Every folder found gets used
+    to call recursiveCall again.
+
+    function: function of one argument, gets called with a list of the found
+    files on everylevel.
+    folder: starting folder, gets opened first.
+    filter: function of one input which takes in a filename and returns True
+    if the file should be passed on to function, False otherwise
+    """
+
+    # we are better off working only with absolute paths from now on. This is
+    # just a safety measure essentially, this function does not change an
+    # already absolute path at all.
+    abspath = os.path.abspath(folder)
+    folderContents = os.listdir(abspath)
+
+    files = []
+    folders = []
+    if len(folderContents) != 0:
+        for entry in folderContents:
+            if os.path.isdir(entry):
+                folders.append(entry)
+            elif filter(entry):
+                files.append(entry)
+
+        # makes the contents list a list containing full paths.
+        files = [abspath + PSEP + path for path in files]
+        if len(files) > 0:
+            function(files)
+
+        folders = [abspath + PSEP + path for path in folders]
+        if len(folders) > 0:
+            for newFolder in folders:
+                recursiveCall(function, filter, newFolder)
+    return
